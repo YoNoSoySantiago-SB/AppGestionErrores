813cd43c00dac96ddee11f47e9efa5e91dc9340f
carolina.pasuy@segurosbolivar.com-Techcamp <124597917+carolinapasuy@users.noreply.github.com>
2023-02-21 12:44:00 -0500

    The index.ts was configured

--- Code/FrontendLibrary/EventLogs/index.d.ts
+++ Code/FrontendLibrary/EventLogs/index.d.ts
@@ -2,4 +2,6 @@ declare module 'event-logs' {
     export function saveRequestHTTP(request:string): void;
     export function saveNavigation(navigation:string): void;
     export function saveError(id:number): void;
+    export async function persistAplicacionErrorFrontEnd(aplicacionError:Object,trazabilidadError:Object,accionesUsuario:Object[]):Promise<void>;
+    export async function saveTrazabilitiyandUserevents(idaplicacionError:number,trazabilidadError:Object,accionesUsuario: Object[]):Promise<void>;
   }
